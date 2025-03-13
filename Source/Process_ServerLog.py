bbc2369a7d5cd48014673f704c7195dc61e7136e
unknown <kanna28pgp@gmail.com>
2025-03-13 16:19:57 +0530

    Completed the code

--- Source/Process_ServerLog.py
+++ Source/Process_ServerLog.py
@@ -13,8 +13,8 @@ def mongo_SetUp():
   myMongouri = credentials.Mongouri
   #Create a new client and connect to the server
   myMongoClient = MongoClient(myMongouri, server_api=ServerApi('1'))
-  myMongodb = myMongoClient.test
-  records = myMongodb.Project3
+  myMongodb = myMongoClient.ProjectDB
+  records = myMongodb.test
   return records
 
 #MySQL Setup
@@ -26,7 +26,7 @@ def mySql_SetUp():
 # Function to create the SQLite table based on the MongoDB data structure
 def create_table(cursor):
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS user_history2 (
+        CREATE TABLE IF NOT EXISTS user_history3 (
             id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
             MailID TEXT,
             MailDate DATETIME
@@ -34,6 +34,12 @@ def create_table(cursor):
     ''')
     #mySQLconnection.commit()
 
+def fetch_Records(mySQLcursor, Qry):
+    mySQLcursor.execute(Qry)
+    out = mySQLcursor.fetchall()
+    from tabulate import tabulate
+    print(tabulate(out,headers=[i[0] for i in mySQLcursor.description],
+showindex="always",tablefmt='psql'))
 
 myMongoRecords = mongo_SetUp()
 mySQLconnection = mySql_SetUp() 
@@ -59,7 +65,7 @@ unique_maillist = []
 [unique_maillist.append(x) for x in lstMail if x not in unique_maillist]
 
 #Inserting records to MongoDB
-#records.insert_many(unique_maillist)
+myMongoRecords.insert_many(unique_maillist)#uncomment
 
 #Selecting the records from MongoDB
 selMailData = myMongoRecords.find({},{"_id": 0})
@@ -70,17 +76,23 @@ for mailData in selMailData:
   eMailID =  mailData.get("MailID", None)
   eMailDate = mailData.get("Date", None)
 
-  insert_query = "INSERT INTO user_history2 (MailID, MailDate) VALUES (%s, %s)"
+  insert_query = "INSERT INTO user_history3 (MailID, MailDate) VALUES (%s, %s)"
   values = (eMailID, eMailDate)
-  mySQLcursor.execute(insert_query, values)
+  mySQLcursor.execute(insert_query, values) #uncomment
   mySQLconnection.commit()
   #print(f"Inserted document: {mailData}")
+  #print(mailData)
 
-  print(mailData)
+#List all unique email addresses
+fetch_Records(mySQLcursor, "SELECT DISTINCT MailID FROM `user_history3`")
+
+#Count the number of emails received per day
+fetch_Records(mySQLcursor, "SELECT Cast(MailDate as Date) 'Date', COUNT(*) as 'Number of emails' FROM `user_history3` GROUP by Cast(MailDate as Date)")
+
+#Find the first and last email date for each email address
+fetch_Records(mySQLcursor, "SELECT MailID, Min(MailDate) AS 'First Mail Date', MAX(MailDate) as 'Last Mail Date' FROM `user_history3` GROUP by MailID;")
+
+#Count the total number of emails from each domain (e.g., gmail.com, yahoo.com)
+fetch_Records(mySQLcursor, "SELECT SUBSTR(MailID, INSTR( MailID, '@' ) + 1,LENGTH(MailID)) as 'Domain', COUNT(*) as 'Number of Emails' FROM `user_history3` GROUP by SUBSTR(MailID, INSTR( MailID, '@' ) + 1,LENGTH(MailID))")
 
 mySQLconnection.close()
-
-
-
-
-
